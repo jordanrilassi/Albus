f118ed31b85314b485bd2c1478963652af6813cb
Jordan Rilassi <jordan.rilassi@orange.com>
2018-06-01 14:43:26 +0200

    Add Realm

---
+++ GithubTrending/Configuration/GithubTrendingConstants.swift
@@ -0,0 +1,15 @@
+//
+//  GithubTrendingConstants.swift
+//  GithubTrending
+//
+//  Created by KZLW4997 on 01/06/2018.
+//  Copyright © 2018 jordanrilassi. All rights reserved.
+//
+
+import Foundation
+
+enum GithubTrendingConstants {
+    struct API {
+        static let urlString = "https://api.github.com/repositories?language=swift"
+    }
+}
---
+++ GithubTrending/Domain/Entities/GithubRepository.swift
@@ -0,0 +1,9 @@
+//
+//  GithubRepository.swift
+//  GithubTrending
+//
+//  Created by KZLW4997 on 01/06/2018.
+//  Copyright © 2018 jordanrilassi. All rights reserved.
+//
+
+import Foundation
