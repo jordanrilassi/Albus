45d9fe90ccb7ba7ffc020f04b10ae475980f5471
Jordan Rilassi <jordan.rilassi@orange.com>
2018-06-02 11:37:02 +0200

    Add detail page

--- GithubTrending/UI/ViewControllers/GithubRepositoryDetailViewController.swift
+++ GithubTrending/UI/ViewControllers/GithubRepositoryDetailViewController.swift
@@ -17,6 +17,7 @@ class GithubRepositoryDetailViewController : UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let nib = UINib(nibName: GithubTrendingConstants.Cells.detailCellNibName, bundle: nil)
         self.tableView.register(nib, forCellReuseIdentifier: GithubTrendingConstants.Cells.detailCellIdentifier)
     }
@@ -33,18 +34,22 @@ class GithubRepositoryDetailViewController : UITableViewController {
         return GithubTrendingConstants.Config.numberOfCells
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: GithubTrendingConstants.Cells.detailCellIdentifier, for: indexPath) as! GithubDetailCell
-//        if indexPath.row == 0 {
-//            cell.titleLabel.text = "Author :"
-//            cell.contentLabel.text = repository?.owner?.login
-//        } else if indexPath.row == 1 {
-//            cell.titleLabel.text = "Description : "
-//            cell.contentLabel.text = repository?.repoDescription
-//        } else {
-//            cell.titleLabel.text = "Number of contributors : "
-//            cell.contentLabel.text = "\(repository?.contributors ?? 0)"
-//        }
+        if indexPath.row == 0 {
+            cell.titleLabel.text = "Author :"
+            cell.contentLabel.text = repository?.owner?.login
+        } else if indexPath.row == 1 {
+            cell.titleLabel.text = "Description : "
+            cell.contentLabel.text = repository?.repoDescription
+        } else {
+            cell.titleLabel.text = "Number of contributors : "
+            cell.contentLabel.text = "\(repository?.contributors ?? 0)"
+        }
         return cell
     }
 }
